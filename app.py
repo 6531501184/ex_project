85025d702cfab49173836accca207bf1ba42a08d
6531501184 <157331696+6531501184@users.noreply.github.com>
2025-05-04 16:42:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,31 @@ Spyder Editor
 This is a temporary script file.
 """
 
+import pickle
+import streamlit as st
+import numpy as np
+
+# Load model
+with open('random_forest_model.pkl', 'rb') as f:
+    dtm_model = pickle.load(f)
+
+# App title
+st.title("🌼 Music Recommendation")
+st.write("Enter the question of music type:")
+
+# Input fields
+sepal_length = st.slider("Sepal Length (cm)", 1, 2, 3, 4, 5, 6)
+sepal_width = st.slider("Sepal Width (cm)", 1, 2, 3, 4, 5, 6)
+petal_length = st.slider("Petal Length (cm)", 1, 2, 3, 4, 5, 6)
+petal_width = st.slider("Petal Width (cm)", 1, 2, 3, 4, 5, 6)
+
+# Predict button
+if st.button("Predict"):
+    input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = dtm_model.predict(input_data)
+    species = ['1', '2', '3','4', '5', '6']
+    st.success(f"The predicted species is: **{species[prediction[0]]}**")
+
+
 
 
